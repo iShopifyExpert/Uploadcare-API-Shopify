c1332ee761649b541c1d824fa27438dcc86b1dd1
Aaliyan Gul <aaliyan111@gmail.com>
2020-07-20 01:27:31 +0500

    Create translate-button.js

---
+++ translate-button.js
@@ -0,0 +1,16 @@
+<script>
+UPLOADCARE_LOCALE_TRANSLATIONS = {
+  buttons: {
+    choose: {
+      files: {
+        one: 'Upload your CV',
+        other: 'Upload your documents '
+      },
+      images: {
+        one: 'Upload your photo',
+        other: 'Upload your photos'
+      }
+    }
+  }
+};
+</script>
